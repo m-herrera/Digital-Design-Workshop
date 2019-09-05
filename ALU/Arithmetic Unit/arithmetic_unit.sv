91aa196004fe07bf8fd6c61da6306ff2942d0968
Jasson Rodriguez <jassonrm@icloud.com>
2019-09-05 09:49:09 -0600

    Arithmetic unit Carry Out Fix

--- ALU/Arithmetic Unit/arithmetic_unit.sv
+++ ALU/Arithmetic Unit/arithmetic_unit.sv
@@ -5,10 +5,13 @@ module arithmetic_unit #(parameter N=4)
 	output logic [N-1:0] out);
 
 	logic [N-1:0] sum_out, subtract_out, shift_out;
+	logic sum_cout, subtract_cout;
 	
-	adder #(N) Adder(a, b, 0, sum_out, cout);
-	subtract #(N) Subtract(a, b, subtract_out);
+	adder #(N) Adder(a, b, 0, sum_out, sum_cout);
+	subtract #(N) Subtract(a, b, subtract_out, subtract_cout);
 	arithmetic_shift #(N) Shifter(a, b[1:0], shift_out);
 	mux4 #(N) Mux(sum_out, subtract_out, shift_out, 'z, select, out);
+	
+	assign cout = sum_cout | subtract_cout;
 	
 endmodule
