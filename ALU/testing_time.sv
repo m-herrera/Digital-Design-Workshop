0fc5c62e407cab55a6460f06b39b0fd960a1cf17
Kenneth Hernandez <khken21@gmail.com>
2019-09-07 12:52:36 -0600

    Testing fixed
    Time testing fixed, idk what do you want me to say lol

--- ALU/testing_time.sv
+++ ALU/testing_time.sv
@@ -1,17 +1,20 @@
-module testing_time #(parameter N=50)
-	(input logic [N-1:0] a, b,
+module testing_time #(parameter N=258)
+	(input logic [1:0] a, b,
 	input logic [3:0] ALUControl,
 	input logic clock,
 	output logic [N-1:0] out,
 	output logic negative, zero, carry_out, overflow);
 	
+reg [N-1:0] c;
+reg [N-1:0] d;
+	
 always@ (posedge clock)
 begin
-c=a;
-d=b;
-control=ALUControl;
+c<=a;
+d<=b;
+
 end
-arithmetic_logic_unit #(N) alit(c,d,control,out2,negative1, zero1, carry_out1, overflow1);
+arithmetic_logic_unit #(N) alit(c,d,ALUControl,out2,negative1, zero1, carry_out1, overflow1);
 always@ (posedge ~clock)
 begin
 out= out2 ;
