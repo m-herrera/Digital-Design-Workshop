9dfb3589be8da215965b0d8ee3ed05361a2ea924
Jasson Rodriguez <jassonrm@icloud.com>
2019-10-10 11:21:49 -0600

    Fix renderer test

--- VGA/src/renderer_test.sv
+++ VGA/src/renderer_test.sv
@@ -1,21 +1,24 @@
+`timescale 10ns/10ns
+
 module renderer_test();
 
 	logic clk, rst, blank, sync;
-	logic[9:0] pixely, pixelx;
 	logic[7:0] vga_r, vga_g, vga_b;
-	logic[1:0] player;
 	logic[3:0] player_address;
+	logic[9:0] hsync_pos, line_size, vsync_pos, frame_size, col, row;
 	
+	synchronizer #(9) DUT(clk, rst, hsync_pos, vsync_pos, line_size, frame_size, col, row, hsync, vsync);
 
-	renderer Rend(clk, rst, pixelx, pixely, player, player_address, vga_r, vga_g, vga_b, blank, sync);
+	renderer Rend(clk, rst, col, row, player_address, vga_r, vga_g, vga_b, blank, sync);
 	
-	initial begin
-		player = 2'b10; pixely = 10'd300; pixelx = 10'd300;
-		
-		#10 pixely = 10'd0; pixelx = 10'd0;
-		
-		#10 pixely = 10'd790; pixelx = 10'd520;
+initial begin
+clk = 0; rst = 0; hsync_pos = 'd95; line_size = 'd793; vsync_pos = 'd2; frame_size = 'd480;
+#4 rst = 1;
+#4 rst = 0;
 	
-	end
+end
+	
+always
+#2 clk <= !clk;
 
 endmodule 
