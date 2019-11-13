dcd9228d8412fb0ea96e803c06f979a2e990bdc1
Jasson Rodriguez <jassonrm@icloud.com>
2019-11-12 23:52:59 -0600

    Fix ALU's logic unit

--- ALU/Logic Unit/logic_unit.sv
+++ ALU/Logic Unit/logic_unit.sv
@@ -1,11 +1,11 @@
-module logic_unit 
-	#(parameter N = 8)
-		(input logic [N-1:0] num1, num2,
-		input logic  ALUControl,
-		output logic[N-1:0] result);
-			logic [N-1:0] or_result, and_result;
-			or_gate #(N) Or(num1, num2, or_result); 
-			and_gate #(N) And(num1, num2, and_result);
-			mux2 #(N) Mux(and_result, or_result,AluControl ,result);
+module logic_unit #(parameter N = 8)
+						(input logic [N-1:0] num1, num2,
+						input logic ALUControl,
+						output logic[N-1:0] result);
+		
+	logic [N-1:0] or_result, and_result;
+	or_gate #(N) Or(num1, num2, or_result); 
+	and_gate #(N) And(num1, num2, and_result);
+	mux2 #(N) Mux(and_result, or_result, ALUControl, result);
 			
-	endmodule 
+endmodule 
