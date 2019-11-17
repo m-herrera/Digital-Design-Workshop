333c392309883d686e944e6e6cfc2a6203036abf
Marco Herrera <m.herrera0799@gmail.com>
2019-11-16 23:18:46 -0600

    fix CPU

--- ALU/arithmetic_logic_unit.sv
+++ ALU/arithmetic_logic_unit.sv
@@ -9,7 +9,7 @@ module arithmetic_logic_unit #(parameter N=8)
 	
 	arithmetic_unit #(N) Arithmetic(a, b, ALUControl[0], carryWire, arithmetic_out);
 	logic_unit #(N) Logic(a, b, ALUControl[0], logic_out);
-	mux2v2 #(N) MainMux(logic_out, arithmetic_out, ALUControl[1], out);
+	mux2v2 #(N) MainMux(arithmetic_out, logic_out, ALUControl[1], out);
 	negative_flag Negative(out[N-1], ~ALUControl[1], negative);
 	zero_flag #(N) Zero(out, zero);
 	carry_flag Carry(carryWire, ALUControl, carry_out);
